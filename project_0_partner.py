e47a259a1ee6c5446cdc3eba2c24e09bcbc014d8
MorganG808 <142527277+MorganG808@users.noreply.github.com>
2023-09-05 09:58:43 -0700

    Working on Input A

--- project_0_partner.py
+++ project_0_partner.py
@@ -2,7 +2,22 @@ print("You are off on your space mission! You've just taken off and left the Ear
 userInput = input("Enter A, B or C. A) Fly into a black hole B) Head into empty space or C) Head towards Mars")
 
 if userInput == 'A':
-# Student 1 finishes this code
+    print("So, you wanna fly into a black hole. You think that's a smart idea?"
+    a_input1 = input("Last chance to back out. A) I'm getting out of here B) Nah screw it, I'm going in")
+    if a_input1 == "A"
+        print("Ah you're boring. Directing ship back to home.")
+        a_input2
+
+    
+    if b_input_one == 'A':
+        print("What?!  The flame makes a sharp turn right as you are preparing for your end!")
+        b_input_two = input("Out of the flames emerges a purple alien with three eyes and four arms!  It latches onto your vessel, making lots of alien noises.  Do you let it in? Y/N")
+        if b_input_two == 'Y':
+            print("It asks you to take it to Earth so it can see the ocean and the sky.  You head back home and become besties!")
+        elif b_input_two == 'N':
+            print("You head back home, and carry this guilt with you forever :(")
+    elif b_input_one == 'B':
+        print("The speed of light?!  What made you think you would survive that?  Everyone on the vessel evaporates into thin air!")
 
 elif userInput == 'B':
 # Student 2 finishes this code 
